de9dbb51d1e925b461fca821c93e42662360f43a
Mozammel169649 <mozammel169649@gmail.com>
2023-11-06 15:41:34 +0600

    express setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const server = express();
+
+server.use(express.static("public"))
+server.set('view engine', 'ejs');
+server.set('views', './views')
+
+const port = 5001;
+server.get("/", function (req, res) {
+    return res.send(` 
+              <h2> hellow home </h2> 
+         `)
+})
+
+server.get("/about", function (req, res) {
+    return res.send(` 
+              <h2> hellow about </h2> 
+         `)
+})
+
+server.get("/contact", function (req, res) {
+    return res.send(` 
+              <h2> hellow contact </h2> 
+         `)
+})
+
+server.listen(port , ()=>{
+    console.log(`app is listening on http://127.0.0.1:${port}`);
+})
+
