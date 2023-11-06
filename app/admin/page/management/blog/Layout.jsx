3df452a3c7cddbe5396477dabd8e904847664537
Mozammel169649 <mozammel169649@gmail.com>
2023-11-06 10:24:42 +0600

    admin all

---
+++ app/admin/page/management/blog/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import setup from "../blog/config/setup"
+const {page_title} = setup
+
+function Layout() {
+    return (
+        <>
+            <div class="container-fluid">
+                <div class="page-header">
+                    <h5> {page_title} </h5>
+                </div>
+            </div>
+            <div>
+                <Outlet />
+            </div>
+        </>
+    )
+}
+
+export default Layout
